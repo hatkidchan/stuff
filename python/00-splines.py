e9734d41dbbf2193ec71c2346d0807a28433dae6
hkc <starbfly0915@gmail.com>
2020-06-02 18:34:01 +0300

    Uploaded splines demo
     * Created with pygame using my own wrapper
     * For more details about splines, check OLC video:
       http://www.youtube.com/watch?v=9_aJGUTePYo

---
+++ python/00-splines.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# Spline renderer based on oneLoneCoder's video:
+# http://www.youtube.com/watch?v=9_aJGUTePYo
+from basegame import BaseGame
+from pygame.math import *
+from math import *
+from time import time
+from pygame.locals import *
+import pygame.mouse as mouse
+
+
+SPLINE_CONTROLPOINT = Color('#00fafa')
+SPLINE_CONTROLPOINT_SEL = Color('#fa00af')
+SPLINE_COLOR = Color('#af00fa')
+BACKGROUND_COLOR = Color('#1313137f')
+
+
+def frange(start, end, step=1.0):
+    v = start
+    while v <= end:
+        yield v
+        v += step
+
+
+class SplinesRenderer(BaseGame):
+    __screen_size__ = (800, 500)
+    def ready(self):
+        self.points = [
+                Vector2(10, 41),
+                Vector2(40, 41),
+                Vector2(70, 41),
+                Vector2(100, 41)
+        ]
+        self.dragging_point = None
+        self.panning = None
+        self.looped = False
+        self.spline_step = 0.1
+        self.spline_step_delta = 0
+        self.move_delta = {'x': 0, 'y': 0}
+        
+    @classmethod
+    def get_spline_point(cls, p, t=0, looped=False):
+        _t = t
+        t = max(min(t, len(p)), 0)
+        if not looped:
+            p1 = min(int(t) + 1, len(p) - 1)
+            p2 = min(p1 + 1, len(p) - 1)
+            p3 = min(p2 + 1, len(p) - 1)
+            p0 = p1 - 1
+        else:
+            p1 = int(t) % len(p)
+            p2 = (p1 + 1) % len(p)
+            p3 = (p1 + 2) % len(p)
+            p0 = p1 - 1
+    
+        t = t % 1
+        tt = t ** 2
+        ttt = t ** 3
+        
+        q1 = -ttt + 2 * tt - t
+        q2 = 3 * ttt - 5 * tt + 2
+        q3 = -3 * ttt + 4 * tt + t
+        q4 = ttt - tt
+        
+        tx = (p[p0].x * q1 + p[p1].x * q2 + p[p2].x * q3 + p[p3].x * q4) / 2
+        ty = (p[p0].y * q1 + p[p1].y * q2 + p[p2].y * q3 + p[p3].y * q4) / 2
+        return Vector2(tx, ty)
+    
+    def frame(self, delta):
+        self.offset_x += self.move_delta['x'] * delta
+        self.offset_y += self.move_delta['y'] * delta
+        
+        if self.spline_step_delta != 0:
+            self.spline_step *= (1 + self.spline_step_delta * delta)
+            self.debug['Step'] = f'{self.spline_step:7.5f}'
+
+        self.draw.rect(BACKGROUND_COLOR, self.surface.get_rect(), screen=True)
+        
+        start = time()
+        spline = []
+        limit = len(self.points) - (0 if self.looped else 3)
+        for t in frange(0, limit, self.spline_step):
+            spline.append(self.get_spline_point(self.points, t, self.looped))
+        self.debug['Calc'] = f'{time() - start:7.5f}s'
+        self.debug['nLines'] = len(spline)
+        
+        start = time()
+        if len(spline) > 1:
+            self.draw.lines(SPLINE_COLOR, spline,
+                            width=int(2 * self.scale), closed=self.looped)
+        self.debug['Render'] = f'{time() - start:7.5f}s'
+
+        mwpos = Vector2(self.screen_to_world(*mouse.get_pos()))
+        for point in self.points:
+            near = mwpos.distance_to(point) < 5
+            color = SPLINE_CONTROLPOINT_SEL if near else SPLINE_CONTROLPOINT
+            self.draw.circle(color, point, max(int(5 * self.scale), 1), 1)
+    
+    def event_handler(self, event):
+        if event.type == MOUSEBUTTONDOWN:
+            world_mouse_pos = Vector2(self.screen_to_world(*event.pos))
+            if event.button == 1:  # LMB
+                for i, p in enumerate(self.points):
+                    if p.distance_to(world_mouse_pos) < 5:
+                        self.dragging_point = i
+                        break
+                else:
+                    self.panning = (event.pos, (self.offset_x, self.offset_y))
+            elif event.button == 2:  # MMB
+                for i, p in enumerate(self.points):
+                    if p.distance_to(world_mouse_pos) < 5 / self.scale:
+                        self.points.pop(i)
+                        break
+            elif event.button == 3:  # RMB
+                self.points.append(world_mouse_pos)
+            elif event.button == 4:
+                self.zoom_around(*event.pos, 0.1)
+                self.debug['Zoom'] = f'x{self.scale:7.5f}'
+            elif event.button == 5:
+                self.zoom_around(*event.pos, -0.1)
+                self.debug['Zoom'] = f'x{self.scale:7.5f}'
+            else:
+                print('UNHANDLED', event)
+
+        elif event.type == MOUSEBUTTONUP:
+            if self.dragging_point is not None:
+                self.dragging_point = None
+            elif self.panning is not None:
+                self.panning = None
+            else:
+                print('UNHANDLED', event)
+
+        elif event.type == MOUSEMOTION:
+            if self.dragging_point is not None:
+                world_mouse_pos = Vector2(self.screen_to_world(*event.pos))
+                self.points[self.dragging_point] = world_mouse_pos
+            elif self.panning is not None:
+                mdx = self.panning[0][0] - event.pos[0]
+                mdy = self.panning[0][1] - event.pos[1]
+                self.offset_x = self.panning[1][0] + mdx / self.scale
+                self.offset_y = self.panning[1][1] + mdy / self.scale
+            else:
+                print('UNHANDLED', event)
+
+        elif event.type == KEYDOWN:
+            if event.key == K_z:
+                self.spline_step_delta = -0.1
+            elif event.key == K_x:
+                self.spline_step_delta = 0.1
+            elif event.key == K_l:
+                self.looped = not self.looped
+            elif event.key == K_i:
+                self.points.append(Vector2(self.screen_to_world(*mouse.get_pos())))
+            elif event.key == K_UP:
+                self.move_delta['y'] = -100
+            elif event.key == K_DOWN:
+                self.move_delta['y'] = 100
+            elif event.key == K_LEFT:
+                self.move_delta['x'] = -100
+            elif event.key == K_RIGHT:
+                self.move_delta['x'] = 100
+            elif event.key == K_ESCAPE:
+                self.stop()
+            else:
+                print('UNHANDLED', event)
+
+        elif event.type == KEYUP:
+            if event.key in [K_z, K_x]:
+                self.spline_step_delta = 0
+            elif event.key in [K_UP, K_DOWN]:
+                self.move_delta['y'] = 0
+            elif event.key == [K_LEFT, K_RIGHT]:
+                self.move_delta['x'] = 0
+            else:
+                print('UNHANDLED', event)
+        else:
+            print('UNHANDLED', event)
+    
+
+if __name__ == '__main__':
+    game = SplinesRenderer()
+    game.mainloop()
+
---
+++ python/basegame.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+import pygame
+from pygame import Color, Rect, draw
+import pygame.draw as draw
+import pygame.mouse as mouse
+from pygame.locals import *
+from pygame.math import *
+from traceback import format_exc
+
+
+EMPTY = Color('#00000000')
+RED = Color('#ff0000ff')
+BLACK = Color('#000000ff')
+
+
+class AvgCollector:
+    def __init__(self, initial=1, samples=10):
+        self.samples = [initial for _ in range(samples)]
+        self.nsamples = samples
+
+    def push(self, value):
+        self.samples.append(value)
+        if len(self.samples) > self.nsamples:
+            self.samples.pop(0)
+
+    def value(self):
+        return sum(self.samples) / len(self.samples)
+
+
+class TSurface(pygame.Surface):
+    pass
+
+
+class GameDraw:
+    def __init__(self, game):
+        self._game = game
+
+    def _vec2_to_screen(self, vec2):
+        vec2 = Vector2(vec2)
+        x, y = map(int, self._game.world_to_screen(vec2.x, vec2.y))
+        return x, y
+
+    def _rect_to_screen(self, rect):
+        rect = Rect(rect)
+        scale = self._game.scale
+        x, y = map(int, self._game.world_to_screen(rect.x, rect.y))
+        w, h = map(int, [rect.width * scale, rect.height * scale])
+        return Rect(x, y, w, h)
+
+    def rect(self, color, rect, width=0, border_radius=0, screen=False):
+        rect = rect if screen else self._rect_to_screen(rect)
+        return draw.rect(self._game.surface, color, rect, width)
+
+    def polygon(self, color, points, width=0, screen=False):
+        points = points if screen else list(map(self._vec2_to_screen, points))
+        return draw.rect(self._game.surface, color, points, width)
+        
+    def circle(self, color, center, radius, width=0, screen=False):
+        center = center if screen else self._vec2_to_screen(center)
+        return draw.circle(self._game.surface, color, center, radius, width)
+        
+    def ellipse(self, color, rect, width=0, screen=False):
+        rect = rect if screen else self._rect_to_screen(rect)
+        return draw.ellipse(self._game.surface, color, rect, width)
+        
+    def arc(self, color, rect, start, stop, width=1, screen=False):
+        rect = rect if screen else self._rect_to_screen(rect)
+        return draw.arc(self._game.surface, color, rect, start, stop, width)
+        
+    def line(self, color, start, end, width=1, screen=False):
+        start = start if screen else self._vec2_to_screen(start)
+        end = end if screen else self._vec2_to_screen(end)
+        return draw.line(self._game.surface, color, start, end, width)
+
+    def lines(self, color, points, closed=False, width=1, screen=False):
+        points = points if screen else list(map(self._vec2_to_screen, points))
+        return draw.lines(self._game.surface, color, closed, points, width)
+
+    def aaline(self, color, start, end, width=1, screen=False):
+        start = start if screen else self._vec2_to_screen(start)
+        end = end if screen else self._vec2_to_screen(end)
+        return draw.aaline(self._game.surface, color, start, end, width)
+
+    def aalines(self, color, points, closed=False, width=1, screen=False):
+        points = points if screen else list(map(self._vec2_to_screen, points))
+        return draw.aalines(self._game.surface, color, closed, points, width)
+
+
+class BaseGame():
+    __screen_size__ = None
+    def __init__(self, size=(400, 400), fps_limit=120):
+        if not pygame.font.get_init():
+            pygame.font.init()
+        if self.__screen_size__ is not None:
+            size = self.__screen_size__
+        self.__screen = pygame.display.set_mode(size)
+        self.surface = TSurface(size, pygame.SRCALPHA)
+        self.surface_d = TSurface(size, pygame.SRCALPHA)
+        self.font_d = pygame.font.SysFont('monospace', 14)
+        self.clock = pygame.time.Clock()
+        self._fps_limit = fps_limit
+        self.__avg_fps = AvgCollector(0, int(self._fps_limit * 0.5))
+        self.debug = {}
+        self.__stop = False
+        self.__lock = False
+        self._n_frames = 0
+        setattr(self.surface, '_GAME', self)
+        self.scale = 1
+        self.offset_x = 0
+        self.offset_y = 0
+        self.draw = GameDraw(self)
+        self.ready()
+
+    def mainloop(self):
+        while not self.__stop:
+            try:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.__stop = True
+                        return
+                    if not self.__lock:
+                        self.event_handler(event)
+                    elif event.type == pygame.KEYDOWN:
+                        self.__stop = True
+                        return
+            except Exception as e:
+                self.draw_error(e, 'event handler')
+                self.__lock = True
+            
+            if not self.__lock:
+                try:
+                    frame_delta = self.clock.tick(self._fps_limit) / 1000
+                    self.frame(frame_delta)
+                    self.__avg_fps.push(1 / frame_delta)
+                except Exception as e:
+                    self.draw_error(e, 'renderer')
+                    self.__lock = True
+            
+            if not self.__lock:
+                self.debug['FPS'] = f'{self.__avg_fps.value():5.1f}'
+
+                draw.rect(self.surface_d, EMPTY, self.surface_d.get_rect())
+                for i, (k, v) in enumerate(list(self.debug.items())):
+                    if isinstance(v, tuple):
+                        v, color = v
+                    else:
+                        color = Color('#ffffff')
+                    line = self.font_d.render(f'{k}: {v}', True, color)
+                    self.surface_d.blit(line, (0, i * 16))
+            
+                self.__screen.blit(self.surface, (0, 0))
+            self.__screen.blit(self.surface_d, (0, 0))
+            pygame.display.flip()
+            self._n_frames += 1
+    
+    def ready(self):
+        pass
+
+    def frame(self, delta):
+        draw.rect(self.surface, RED, self.surface.get_rect())
+        self.draw.line(BLACK, Vector2(0, 0),
+                       Vector2(self.screen_to_world(*mouse.get_pos())))
+
+    def event_handler(self, event):
+        print(event.type, event)
+    
+    def stop(self):
+        self.__stop = True
+        
+    def world_to_screen(self, wx, wy):
+        return ((wx - self.offset_x) * self.scale,
+                (wy - self.offset_y) * self.scale)
+
+    def screen_to_world(self, sx, sy):
+        return ((sx / self.scale) + self.offset_x,
+                (sy / self.scale) + self.offset_y)
+            
+    def zoom_around(self, mx, my, factor):
+        owmx, owmy = self.screen_to_world(mx, my)
+        self.scale *= (1 + factor)
+        nwmx, nwmy = self.screen_to_world(mx, my)
+        self.offset_x += (owmx - nwmx)
+        self.offset_y += (owmy - nwmy)
+
+    def draw_error(self, e, block='<unknown>'):
+        draw.rect(self.surface_d, Color('#131313'), self.surface_d.get_rect())
+        lines = []
+        lines.append(f'{e!r} occurred in {block}')
+        lines += format_exc().split('\n')
+        for i, line in enumerate(lines):
+            print(line)
+            surf = self.font_d.render(line, True, RED, Color('#131313'))
+            self.surface_d.blit(surf, (0, i * 16))
+
+if __name__ == '__main__':
+    game = BaseGame()
+    game.mainloop()
+
